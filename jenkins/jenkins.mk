ad95f16a6e576824a808f167bf8d24d38c92220b
Brian Brietzke <brian.brietzke@symphony.com>
2019-04-26 13:30:45 -0500

    A couple of updates to the jenkins builds.

--- jenkins/jenkins.mk
+++ jenkins/jenkins.mk
@@ -5,5 +5,9 @@ include **/*.mk
 $(PACKER): /usr/local/bin/packer
 	brew install packer
 
+$(JQ): /usr/local/bin/jq
+	brew install jq
+
 jenkins: $(PACKER)
 	cd jenkins && packer build build.json
+	cat jenkins.manifest | jq -r .builds[0].artifact_id |  cut -d':' -f2 > /tmp/ami.txt
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -11,3 +11,14 @@ sudo yum install -y git jq groovy java-1.8.0-openjdk jenkins
 sudo service jenkins start
 
 sudo chkconfig jenkins on
+
+wget -O $HOME/packer.zip https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_freebsd_amd64.zip
+wget -O $HOME/terraform.zip https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+
+unzip $HOME/packer.zip
+unzip $HOME/terraform.zip
+
+sudo mv $HOME/packer /usr/local/bin/packer
+sudo mv $HOME/terraform /usr/local/bin/terraform
+
+rm $HOME/*.zip
